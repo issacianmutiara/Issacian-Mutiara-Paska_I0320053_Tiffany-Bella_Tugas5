b9e10f5457c93e0495933f7b2a3067893b080a56
I Mutiara <issacianmutiara@gmail.com>
2021-03-29 22:25:29 +0700

    I0320053_Exercise 5.1

---
+++ I0320053_Exercise 5.1.py
@@ -0,0 +1,9 @@
+#penggunaan if untuk satu kasus
+#menginput bilangan
+bilangan = int(input('Masukkan bilangan bulat: '))
+
+#memeriksa bilangan
+if bilangan % 4 == 0 :
+    print('%d adalah bilangan genap' % bilangan)
+
+print()
